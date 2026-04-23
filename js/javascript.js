b32cd46ea7b4dbda385584bf587b660dddac90bd
Min Naing Oo <minnaingoo792@gmail.com>
2026-04-24 00:31:57 +0630

    table

--- js/javascript.js
+++ js/javascript.js
@@ -6,3 +6,50 @@ menutoggle.addEventListener('click', ()=>{
     console.log("Menu Clicked");
     
 })
+
+
+// order form 
+const table = document.querySelector('#table');
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(response => response.json())
+.then(data => {
+    console.table(data);
+    let rows = `
+    <thead> 
+               <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Address</th>
+                <th>Phone</th>
+               </tr>
+            </thead>
+    `;
+    data.forEach(user => {
+    rows += `<tr>
+    <td>${user.id}</td>
+    <td>${user.name}</td>
+    <td>${user.email}</td>
+    <td>${user.address.city}</td>
+    <td>${user.phone}</td>
+    </tr>`
+       
+    table.innerHTML = rows
+       
+    });
+})
+
+
+// const table = document.querySelector('#table')
+//         fetch("https://jsonplaceholder.typicode.com/users")
+//             .then(resposne => resposne.json())
+//             .then(data => {
+//                 console.log(data);
+//                 let rows = ""
+//                 data.forEach(user => {
+//                     rows += `<tr><td>${user.name}</td><td>${user.email}</td></tr>`
+//                 });
+//                 table.innerHTML = rows
+//             })
