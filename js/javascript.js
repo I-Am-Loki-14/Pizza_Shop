584eb565beb00999392b81fc956ae3b49c7a4533
Min Naing Oo <minnaingoo792@gmail.com>
2026-04-24 00:48:38 +0630

    table_remove

--- js/javascript.js
+++ js/javascript.js
@@ -9,37 +9,37 @@ menutoggle.addEventListener('click', ()=>{
 
 
 // order form 
-const table = document.querySelector('#table');
+// const table = document.querySelector('#table');
 
 
-fetch("https://jsonplaceholder.typicode.com/users")
-.then(response => response.json())
-.then(data => {
-    console.table(data);
-    let rows = `
-    <thead> 
-               <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Address</th>
-                <th>Phone</th>
-               </tr>
-            </thead>
-    `;
-    data.forEach(user => {
-    rows += `<tr>
-    <td>${user.id}</td>
-    <td>${user.name}</td>
-    <td>${user.email}</td>
-    <td>${user.address.city}</td>
-    <td>${user.phone}</td>
-    </tr>`
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then(response => response.json())
+// .then(data => {
+//     console.table(data);
+//     let rows = `
+//     <thead> 
+//                <tr>
+//                 <th>ID</th>
+//                 <th>Name</th>
+//                 <th>Email</th>
+//                 <th>Address</th>
+//                 <th>Phone</th>
+//                </tr>
+//             </thead>
+//     `;
+//     data.forEach(user => {
+//     rows += `<tr>
+//     <td>${user.id}</td>
+//     <td>${user.name}</td>
+//     <td>${user.email}</td>
+//     <td>${user.address.city}</td>
+//     <td>${user.phone}</td>
+//     </tr>`
        
-    table.innerHTML = rows
+//     table.innerHTML = rows
        
-    });
-})
+//     });
+// })
 
 
 // const table = document.querySelector('#table')
